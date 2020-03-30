51ca1e6b3ae0db2aa631b9f6c4c02f09a901bdc0
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-03-30 11:05:18 -0700

    chore: use typescript

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-var express = require('express');
+import express from "express";
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
