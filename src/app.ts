06360e305527de9dbdc064663654349ef7fd6895
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-03-30 18:40:01 -0700

    feat: add semantic-ui to public

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../src/public')));
+app.use('/semantic', express.static(path.join(__dirname, '../semantic/dist')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
