95f4eacc605f5ee9d6ca87b3abc65ce04b89762f
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-04-29 18:37:25 -0700

    feat: show basic guild stats

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,11 +1,38 @@
-import { LitElement, customElement, html, TemplateResult, CSSResult, css } from "lit-element";
+import { LitElement, customElement, html, TemplateResult, CSSResult, css, property } from "lit-element";
 
 @customElement("app-view")
 class App extends LitElement {
+    @property({ type: Object }) private guildData: any | null = null;
+    @property({ type: Number }) private numMembers: number | string = "...";
+    @property({ type: String }) private guildExpStr: string = "...";
+
+    public constructor() {
+        super();
+        (async () => {
+            const res = await (await fetch("/api/guild")).json();
+            this.guildData = res;
+            console.log(this.guildData);
+            this.numMembers = this.guildData.guild.members.length;
+            this.guildExpStr = `${Math.round(res.guild.exp / 1000000)}M`;
+        })();
+    }
+
     static get styles(): CSSResult {
         return css`
             * {
                 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+            }
+
+            h3 {
+                font-weight: inherit;
+            }
+
+            h3 span {
+                font-weight: bold;
+            }
+
+            .App-right {
+                float: right;
             }
         `;
     }
@@ -13,6 +40,12 @@ class App extends LitElement {
     protected render(): TemplateResult {
         return html`
             <h1>Bloody Bedwars</h1>
+            <h3>
+                <span>${this.numMembers}</span> Members
+                <div class="App-right">
+                    <span>${this.guildExpStr}</span> Guild Experience
+                </div>
+            </h3>
         `;
     }
 }
